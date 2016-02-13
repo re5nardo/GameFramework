8e5f9d7872c9bdeedec592bfccba76f9294e31a6
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-02-13 12:59:36 +0000

    테스트용 스크립트

--- Client_Root/Client/Assets/Scripts/BaeTest.cs
+++ Client_Root/Client/Assets/Scripts/BaeTest.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Text;
 
 public class BaeTest : MonoBehaviour
 {
@@ -12,20 +14,29 @@ public class BaeTest : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			TestMessage testMsg = new TestMessage ();
+			testMsg.SetName ("배");
+			testMsg.SetAge (34);
+			testMsg.m_listFavoriteNumbers.Add (7);
+			testMsg.m_dicOptions.Add ("햄스터", "햄메돌");
+
+			Network.Instance.Send (testMsg);	
+		}
 	}
 
 	private void OnConnected(bool bSuccess)
 	{
 		if (bSuccess)
 		{
-			TestMessage testMsg = new TestMessage ();
-			testMsg.SetName ("Bae");
-			testMsg.SetAge (34);
-			testMsg.m_listFavoriteNumbers.Add (7);
-			testMsg.m_dicOptions.Add ("햄스터", "햄메돌");
-
-			Network.Instance.Send (testMsg);
+//			TestMessage testMsg = new TestMessage ();
+//			testMsg.SetName ("Bae");
+//			testMsg.SetAge (34);
+//			testMsg.m_listFavoriteNumbers.Add (7);
+//			testMsg.m_dicOptions.Add ("햄스터", "햄메돌");
+//
+//			Network.Instance.Send (testMsg);
 		}
 	}
 }
