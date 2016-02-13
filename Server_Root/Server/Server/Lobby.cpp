02aa22a4b699c1e7d85342b6ba787d55defb2748
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-02-13 13:14:00 +0000

    로비 & 네트워크 모듈 연결 로직 구현

--- Server_Root/Server/Server/Lobby.cpp
+++ Server_Root/Server/Server/Lobby.cpp
@@ -1,12 +1,39 @@
 #include "stdafx.h"
 #include "Lobby.h"
+#include "Network.h"
+#include "IMessage.h"
+#include "TestMessage.h"
+#include "NetworkDefines.h"
 
 
-Lobby::Lobby()
+Lobby::Lobby(const unsigned short nPort)
 {
+	m_pNetwork = new Network(nPort);
+	m_pNetwork->SetRecvMessageCallback(OnRecvMessage);
+
+	m_pNetwork->Start();
 }
 
 
 Lobby::~Lobby()
 {
+	m_pNetwork->Stop();
+	delete m_pNetwork;
+}
+
+void Lobby::OnRecvMessage(unsigned int socket, IMessage* pMsg)
+{
+	if (pMsg == NULL)
+	{
+		return;
+	}
+
+	if (pMsg->GetID() == Messages::TEST_MESSAGE_ID)
+	{
+		//	temp
+		TestMessage* pTestMsg = (TestMessage*)pMsg;
+		printf("%s", pTestMsg->Serialize().c_str());
+	}
+
+	delete pMsg;
 }
--- Server_Root/Server/Server/Lobby.h
+++ Server_Root/Server/Server/Lobby.h
@@ -1,8 +1,18 @@
 #pragma once
+
+class Network;
+class IMessage;
+
 class Lobby
 {
 public:
-	Lobby();
-	~Lobby();
+	Lobby(const unsigned short nPort);
+	virtual ~Lobby();
+
+private:
+	Network*	m_pNetwork;
+
+private:
+	static void OnRecvMessage(unsigned int socket, IMessage* pMsg);
 };
 
