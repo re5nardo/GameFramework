1e70d98d52e252c3f3d6ba36953cb67a05857ac9
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-02-15 12:18:03 +0000

    이동 관련 테스트 로직 추가

--- Client_Root/Client/Assets/Scripts/BaeTest.cs
+++ Client_Root/Client/Assets/Scripts/BaeTest.cs
@@ -8,12 +8,12 @@ public class BaeTest : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{	
-		
 		Network.Instance.ConnectToServer ("127.0.0.1", 9110, OnConnected, OnRecvMessage);
 	}
 
-	private float speed = 0.1F;
-	public GameObject goCharacter;
+	private Coroutine curCoroutine;
+	private float speed = 200;
+	public GameObject goCube;
 	private Vector3 vec3Dest;
 
 	// Update is called once per frame
@@ -71,5 +71,42 @@ public class BaeTest : MonoBehaviour
 	private void OnRecvMessage(IMessage msg)
 	{
 		Debug.Log (msg.Serialize());
+
+		if (msg.GetID () == (ushort)Messages.REQ_MOVE_ID)
+		{
+			ReqMove moveMsg = msg as ReqMove;
+			vec3Dest = new Vector3(moveMsg.m_vec3Position.x, moveMsg.m_vec3Position.y, 0f);
+
+			if (curCoroutine != null)
+			{
+				StopCoroutine (curCoroutine);
+			}
+			curCoroutine = StartCoroutine (MoveCoroutine ());
+		}
+	}
+
+	IEnumerator MoveCoroutine()
+	{
+		Vector3 vec3StartPos = goCube.transform.position;
+		Vector3 vec3Nor = (vec3Dest - vec3StartPos).normalized;
+
+		float fReqTime = (vec3Dest - vec3StartPos).magnitude / speed;
+		float fDeltaTime = 0f;
+
+		while (true)
+		{
+			fDeltaTime += Time.deltaTime;
+
+			goCube.transform.position = vec3StartPos + vec3Nor * speed * fDeltaTime;
+
+			if (fDeltaTime >= fReqTime)
+			{
+				break;
+			}
+
+			yield return null;;
+		}
+
+		curCoroutine = null;
 	}
 }
