61f5c379c7d4565225858ac95160a8843ce2f011
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-03-20 11:35:26 +0000

    SparseGraph 구현

---
+++ Client_Root/Client/Assets/Scripts/Navigation/IGraph.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public interface IGraph<node_type, edge_type> where node_type : INode where edge_type : IEdge
+{
+	int NumNodes ();
+	LinkedList<edge_type> GetEdgesOfNode (int node);
+	node_type GetNode(int node);
+}
