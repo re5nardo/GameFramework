8ae4165f028ec21f09e93f68fe1c4a0a31f7e574
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-03-20 11:36:30 +0000

    AStart 알고리즘 구현

---
+++ Client_Root/Client/Assets/Scripts/Navigation/GraphAlgorithms/AStarHeuristicPolicies.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------------
+//
+//  Name:   AStarHeuristicPolicies.h
+//
+//  Author: Mat Buckland (www.ai-junkie.com)
+//
+//  Desc:   class templates defining a heuristic policy for use with the A*
+//          search algorithm
+//-----------------------------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+public interface IHeuristic
+{
+	double Calculate<graph_type, node_type, edge_type, extra_info> (graph_type G, int nd1, int nd2) where graph_type : IGraph<node_type, edge_type> where node_type : NavGraphNode<extra_info> where edge_type : IEdge;
+}
+
+//-----------------------------------------------------------------------------
+//the euclidian heuristic (straight-line distance)
+//-----------------------------------------------------------------------------
+class Heuristic_Euclid : IHeuristic
+{
+	public Heuristic_Euclid(){}
+
+	//calculate the straight line distance from node nd1 to node nd2
+	public double Calculate<graph_type, node_type, edge_type, extra_info>(graph_type G, int nd1, int nd2) where graph_type : IGraph<node_type, edge_type> where node_type : NavGraphNode<extra_info> where edge_type : IEdge
+	{
+		return Vector2.Distance (G.GetNode (nd1).Pos (), G.GetNode (nd2).Pos ());
+	}
+};
+
+//-----------------------------------------------------------------------------
+//this uses the euclidian distance but adds in an amount of noise to the 
+//result. You can use this heuristic to provide imperfect paths. This can
+//be handy if you find that you frequently have lots of agents all following
+//each other in single file to get from one place to another
+//-----------------------------------------------------------------------------
+class Heuristic_Noisy_Euclidian : IHeuristic
+{
+	public Heuristic_Noisy_Euclidian(){}
+
+	//calculate the straight line distance from node nd1 to node nd2
+	public double Calculate<graph_type, node_type, edge_type, extra_info>(graph_type G, int nd1, int nd2) where graph_type : IGraph<node_type, edge_type> where node_type : NavGraphNode<extra_info>  where edge_type : IEdge
+	{
+		return Vector2.Distance (G.GetNode (nd1).Pos (), G.GetNode (nd2).Pos ()) * Utils.RandInRange(0.9f, 1.1f);
+	}
+};
+
+//-----------------------------------------------------------------------------
+//you can use this class to turn the A* algorithm into Dijkstra's search.
+//this is because Dijkstra's is equivalent to an A* search using a heuristic
+//value that is always equal to zero.
+//-----------------------------------------------------------------------------
+class Heuristic_Dijkstra : IHeuristic
+{
+	public double Calculate<graph_type, node_type, edge_type, extra_info>(graph_type G, int nd1, int nd2) where graph_type : IGraph<node_type, edge_type> where node_type : NavGraphNode<extra_info>  where edge_type : IEdge
+	{
+		return 0;
+	}
+};
---
+++ Client_Root/Client/Assets/Scripts/Navigation/IndexedPriorityQLow.cs
@@ -0,0 +1,128 @@
+﻿//----------------------- IndexedPriorityQLow ---------------------------
+//
+//  Priority queue based on an index into a set of keys. The queue is
+//  maintained as a 2-way heap.
+//
+//  The priority in this implementation is the lowest valued key
+//------------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class IndexedPriorityQLow<KeyType>
+{
+	private List<KeyType>  	m_vecKeys;
+	private List<int>       m_Heap;
+	private List<int>       m_invHeap;
+	private int             m_iSize, m_iMaxSize;
+
+	private void Swap(int a, int b)
+	{
+		int temp = m_Heap[a]; m_Heap[a] = m_Heap[b]; m_Heap[b] = temp;
+
+		//change the handles too
+		m_invHeap[m_Heap[a]] = a; m_invHeap[m_Heap[b]] = b;
+	}
+
+	private void ReorderUpwards(int nd)
+	{
+		if ((typeof(KeyType) is double) == false)
+		{
+			Debug.Assert (true, "exception!");
+		}
+			
+		//move up the heap swapping the elements until the heap is ordered
+		while ((nd > 1) && ((double)(object)m_vecKeys [m_Heap [nd / 2]] > (double)(object)m_vecKeys [m_Heap [nd]]))
+		{      
+			Swap (nd / 2, nd);
+
+			nd /= 2;
+		}
+	}
+
+	private void ReorderDownwards(int nd, int HeapSize)
+	{
+		if ((typeof(KeyType) is double) == false)
+		{
+			Debug.Assert (true, "exception!");
+		}
+
+		//move down the heap from node nd swapping the elements until the heap is reordered
+		while (2*nd <= HeapSize)
+		{
+			int child = 2 * nd;
+
+			//set child to smaller of nd's two children
+			if ((child < HeapSize) && ((double)(object)m_vecKeys[m_Heap[child]] > (double)(object)m_vecKeys[m_Heap[child+1]]))
+			{
+				++child;
+			}
+
+			//if this nd is larger than its child, swap
+			if ((double)(object)m_vecKeys[m_Heap[nd]] > (double)(object)m_vecKeys[m_Heap[child]])
+			{
+				Swap(child, nd);
+
+				//move the current node down the tree
+				nd = child;
+			}
+
+			else
+			{
+				break;
+			}
+		}
+	}
+		
+	//you must pass the constructor a reference to the std::vector the PQ
+	//will be indexing into and the maximum size of the queue.
+	public IndexedPriorityQLow(List<KeyType> keys, int MaxSize)
+	{
+		m_vecKeys = keys;
+		m_iMaxSize = MaxSize;
+		m_iSize = 0;
+		m_Heap = new List<int> ();
+		for(int i = 0; i < MaxSize+1; ++i)
+		{
+			m_Heap.Add (0);
+		}
+		m_invHeap = new List<int> ();
+		for(int i = 0; i < MaxSize+1; ++i)
+		{
+			m_invHeap.Add (0);
+		}
+	}
+
+	public bool empty(){return (m_iSize==0);}
+
+	//to insert an item into the queue it gets added to the end of the heap
+	//and then the heap is reordered from the bottom up.
+	public void insert(int idx)
+	{
+		Debug.Assert (m_iSize+1 <= m_iMaxSize);
+
+		++m_iSize;
+
+		m_Heap[m_iSize] = idx;
+
+		m_invHeap[idx] = m_iSize;
+
+		ReorderUpwards(m_iSize);
+	}
+
+	//to get the min item the first element is exchanged with the lowest
+	//in the heap and then the heap is reordered from the top down. 
+	public int Pop()
+	{
+		Swap(1, m_iSize);
+
+		ReorderDownwards(1, m_iSize-1);
+
+		return m_Heap[m_iSize--];
+	}
+
+	//if the value of one of the client key's changes then call this with the key's index to adjust the queue accordingly
+	public void ChangePriority(int idx)
+	{
+		ReorderUpwards(m_invHeap[idx]);
+	}
+}
