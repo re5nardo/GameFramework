f46d0d266eb76c2e87ea4d54e3a1a21e57db59cb
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-05-22 14:31:10 +0000

    vector3 핸들러 추가

--- Client_Root/Client/Assets/Scripts/Common/Defines.cs
+++ Client_Root/Client/Assets/Scripts/Common/Defines.cs
@@ -1,5 +1,6 @@
-﻿
+﻿using UnityEngine;
 
 public delegate void DefaultHandler();
 public delegate void BoolHandler(bool bValue);
+public delegate void Vector3Handler(Vector3 vec3);
 public delegate void MessageHandler(IMessage msg);
