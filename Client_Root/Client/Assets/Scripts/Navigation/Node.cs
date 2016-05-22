51b8af79a0979a9d9b3dff2982138651df9e34d3
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-05-22 03:42:15 +0000

    Node 구현

---
+++ Client_Root/Client/Assets/Scripts/Navigation/Node.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node
+{
+    public List<Node>   m_listNeighborNode = new List<Node>();
+
+    private Vector3     m_vec3Pos_ = Vector3.zero;
+    public Vector3      m_vec3Pos
+    {
+        get
+        {
+            return m_vec3Pos_;
+        }
+        set
+        {
+            m_vec3Pos_ = value;
+        }
+    }
+
+    public Node()
+    {
+    }
+
+    public Node(Vector3 vec3Pos)
+    {
+        m_vec3Pos = vec3Pos;
+    }
+}
