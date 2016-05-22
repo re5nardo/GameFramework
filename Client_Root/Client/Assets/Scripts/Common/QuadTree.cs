67bd125c554f34f794af65044024e092cae9d4fc
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-05-22 03:41:35 +0000

    1. QuadTree 구현 2. AStar 구현

---
+++ Client_Root/Client/Assets/Scripts/Common/QuadTree.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class QuadTree
+{
+    //  QuadTree of which level is equal to this has nodes  
+    private const int LEVEL_OF_HAVING_NODES = 5;
+
+    // Boundary of this quad tree
+    private Rect2D boundary = new Rect2D();
+
+    //  level of this quad tree
+    private int level = 0;
+
+    // Nodes in this quad tree node
+    private List<Node> m_listNode = new List<Node>();
+
+    // Children
+    private QuadTree northWest = null;
+    private QuadTree northEast = null;
+    private QuadTree southWest = null;
+    private QuadTree southEast = null;
+
+    public QuadTree(Rect2D boundary, int level)
+    {
+        this.boundary = boundary;
+        this.level = level;
+    }
+
+    // Insert a node into the QuadTree
+    public bool Insert(Node node)
+    {
+        // Ignore objects that do not belong in this quad tree
+        if (!boundary.Contains(new Vector2(node.m_vec3Pos.x, node.m_vec3Pos.z)))
+            return false; // object cannot be added
+
+        if (level >= LEVEL_OF_HAVING_NODES)
+        {
+            m_listNode.Add(node);
+
+            return true;
+        }
+        else
+        {
+            if (HasChild())
+            {
+                return InsertIntoChild(node);
+            }
+            else
+            {
+                if (m_listNode.Count > 0)
+                {
+                    Subdivide();
+
+                    foreach(Node nodeOld in m_listNode)
+                    {
+                        InsertIntoChild(nodeOld);
+                    }
+                    m_listNode.Clear();
+
+                    return InsertIntoChild(node);
+                }
+                else
+                {
+                    m_listNode.Add(node);
+
+                    return true;
+                }
+            }
+        }
+    }
+
+    // Create four children that fully divide this quad into four quads of equal area
+    private void Subdivide()
+    {
+        northWest = new QuadTree(new Rect2D(boundary.center.x - boundary.width * 0.25f, boundary.center.y + boundary.height * 0.25f, boundary.width * 0.5f, boundary.height * 0.5f), level + 1);
+        northEast = new QuadTree(new Rect2D(boundary.center.x + boundary.width * 0.25f, boundary.center.y + boundary.height * 0.25f, boundary.width * 0.5f, boundary.height * 0.5f), level + 1);
+        southWest = new QuadTree(new Rect2D(boundary.center.x - boundary.width * 0.25f, boundary.center.y - boundary.height * 0.25f, boundary.width * 0.5f, boundary.height * 0.5f), level + 1);
+        southEast = new QuadTree(new Rect2D(boundary.center.x + boundary.width * 0.25f, boundary.center.y - boundary.height * 0.25f, boundary.width * 0.5f, boundary.height * 0.5f), level + 1);
+    }
+
+    // Find all nodes that appear within a range
+    public List<Node> QueryRange(Rect2D range)
+    {
+        // Prepare an array of results
+        List<Node> nodesInRange = new List<Node>();
+
+        // Automatically abort if the range does not intersect this quad
+        if (!boundary.Overlaps(range))
+            return nodesInRange; // empty list
+
+        // Check objects at this quad level
+        for (int p = 0; p < m_listNode.Count; p++)
+        {
+            if (range.Contains(new Vector2(m_listNode[p].m_vec3Pos.x, m_listNode[p].m_vec3Pos.z)))
+                nodesInRange.Add(m_listNode[p]);
+        }
+
+        // Terminate here, if there are no children
+        if (northWest == null)
+            return nodesInRange;
+
+        // Otherwise, add the points from the children
+        nodesInRange.AddRange(northWest.QueryRange(range));
+        nodesInRange.AddRange(northEast.QueryRange(range));
+        nodesInRange.AddRange(southWest.QueryRange(range));
+        nodesInRange.AddRange(southEast.QueryRange(range));
+
+        return nodesInRange;
+    }
+
+    private bool InsertIntoChild(Node node)
+    {
+        if (northWest.Insert(node))
+            return true;
+        if (northEast.Insert(node))
+            return true;
+        if (southWest.Insert(node))
+            return true;
+        if (southEast.Insert(node))
+            return true;
+
+        // Otherwise, the point cannot be inserted for some unknown reason (this should never happen)
+        return false;
+    }
+
+    private bool HasChild()
+    {
+        return northWest != null;
+    }
+}
+
+//  reference site : https://en.wikipedia.org/wiki/Quadtree
---
+++ Client_Root/Client/Assets/Scripts/Navigation/AStar.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//  reference : https://en.wikipedia.org/wiki/A*_search_algorithm
+public class AStarAlgorithm
+{
+    // The set of nodes already evaluated.
+    LinkedList<Node> closedSet = new LinkedList<Node>();
+
+    // The set of currently discovered nodes still to be evaluated.
+    // Initially, only the start node is known.
+    LinkedList<Node> openSet = new LinkedList<Node>();
+
+    Dictionary<Node, Node> cameFrom = new Dictionary<Node, Node>();
+    Dictionary<Node, float> gScore = new Dictionary<Node, float>();
+    Dictionary<Node, float> fScore = new Dictionary<Node, float>();
+
+    //    f = g + h
+    public LinkedList<Node> AStar(Node start, Node goal)
+    {
+        closedSet.Clear();
+        openSet.Clear();
+        openSet.AddLast(start);
+
+        // For each node, which node it can most efficiently be reached from.
+        // If a node can be reached from many nodes, cameFrom will eventually contain the
+        // most efficient previous step.
+        cameFrom.Clear();
+
+        // For each node, the cost of getting from the start node to that node.
+        gScore.Clear();    //    gScore := map with default value of Infinity
+        // The cost of going from start to start is zero.
+        gScore[start] = 0;
+
+        // For each node, the total cost of getting from the start node to the goal
+        // by passing by that node. That value is partly known, partly heuristic.
+        fScore.Clear();    //fScore := map with default value of Infinity
+        // For the first node, that value is completely heuristic.
+        fScore[start] = heuristic_cost_estimate(start, goal);
+
+        while(openSet.Count != 0)
+        {
+            Node current = FindLowestValue(openSet);
+            if(current.Equals(goal))
+            {
+                return reconstruct_path(current);
+            }
+
+            openSet.Remove(current);
+            closedSet.AddLast(current);
+
+            foreach(Node neighbor in current.m_listNeighborNode)
+            {
+                if(closedSet.Contains(neighbor))
+                {
+                    continue;    // Ignore the neighbor which is already evaluated.
+                }
+
+                // The distance from start to a neighbor
+                float tentative_gScore = gScore[current] + dist_between(current, neighbor);
+                if(!openSet.Contains(neighbor))    // Discover a new node
+                {
+                    openSet.AddLast(neighbor);
+                }
+                else if(tentative_gScore >= gScore[neighbor])
+                {
+                    continue;        // This is not a better path.
+                }
+
+                // This path is the best until now. Record it!
+                cameFrom[neighbor] = current;
+                gScore[neighbor] = tentative_gScore;
+                fScore[neighbor] = gScore[neighbor] + heuristic_cost_estimate(neighbor, goal);
+            }
+        }
+
+        return null; //    failure
+    }
+
+    private Node FindLowestValue(LinkedList<Node> listSrc)
+    {
+        Node nodeFound = null;
+        float fLowestValue = float.MaxValue;
+        foreach(Node point in listSrc)
+        {
+            if(fScore[point] < fLowestValue)
+            {
+                fLowestValue = fScore[point];
+                nodeFound = point;
+            }
+        }
+
+        return nodeFound;
+    }
+
+
+    private LinkedList<Node> reconstruct_path(Node node)
+    {
+        LinkedList<Node> total_path = new LinkedList<Node>();
+
+        Node cur = node;
+        total_path.AddFirst(node);
+        while(cameFrom.ContainsKey(cur))
+        {
+            total_path.AddFirst(cameFrom[cur]);
+            cur = cameFrom[cur];
+        }
+
+        return total_path;
+    }
+
+    private float heuristic_cost_estimate(Node node1, Node node2)
+    {
+        return Mathf.Sqrt(Mathf.Pow((node1.m_vec3Pos.x - node2.m_vec3Pos.x), 2) + Mathf.Pow((node1.m_vec3Pos.z - node2.m_vec3Pos.z), 2));
+    }
+
+    private float dist_between(Node node1, Node node2)
+    {
+        return Mathf.Sqrt(Mathf.Pow((node1.m_vec3Pos.x - node2.m_vec3Pos.x), 2) + Mathf.Pow((node1.m_vec3Pos.z - node2.m_vec3Pos.z), 2));
+    }
+}
