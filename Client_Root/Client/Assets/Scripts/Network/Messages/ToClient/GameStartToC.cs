cd6cee5f905ac42e044fc46faf38cc072ac68c6b
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-06-04 11:27:27 +0000

    게임 시작 관련 네트워크 프로토콜 추가

---
+++ Client_Root/Client/Assets/Scripts/Network/Messages/ToClient/GameStartToC.cs
@@ -0,0 +1,34 @@
+﻿
+public class GameStartToC : IMessage
+{
+    public long m_lGameElapsedTime;     //  json field name : GameElapsedTime
+   
+    public ushort GetID()
+    {
+        return (ushort)Messages.GAME_START_TOC;
+    }
+
+    public string Serialize()
+    {
+        JSONObject jsonObj = new JSONObject (JSONObject.Type.OBJECT);
+        jsonObj.AddField ("GameElapsedTime", m_lGameElapsedTime);
+
+        return jsonObj.Print () + '\0';
+    }
+
+    public bool Deserialize(string strJson)
+    {
+        JSONObject jsonObj = new JSONObject (strJson);
+
+        if (jsonObj.HasField ("GameElapsedTime") && jsonObj.GetField ("GameElapsedTime").IsNumber)
+        {
+            jsonObj.GetField (ref m_lGameElapsedTime, "GameElapsedTime");
+        } 
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Client_Root/Client/Assets/Scripts/Network/Messages/ToServer/ReadyForStartToS.cs
@@ -0,0 +1,34 @@
+﻿
+public class ReadyForStartToS : IMessage
+{
+    public int m_nPlayerIndex;     //  json field name : PlayerIndex
+
+    public ushort GetID()
+    {
+        return (ushort)Messages.READY_FOR_START_TOS;
+    }
+
+    public string Serialize()
+    {
+        JSONObject jsonObj = new JSONObject (JSONObject.Type.OBJECT);
+        jsonObj.AddField ("PlayerIndex", m_nPlayerIndex);
+
+        return jsonObj.Print () + '\0';
+    }
+
+    public bool Deserialize(string strJson)
+    {
+        JSONObject jsonObj = new JSONObject (strJson);
+
+        if (jsonObj.HasField ("PlayerIndex") && jsonObj.GetField ("PlayerIndex").IsNumber)
+        {
+            jsonObj.GetField (ref m_nPlayerIndex, "PlayerIndex");
+        } 
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
