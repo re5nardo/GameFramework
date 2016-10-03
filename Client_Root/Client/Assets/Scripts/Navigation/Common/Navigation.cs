f6dc491774f8c713d94380396cf56e30c0b30aaf
re5nardo <re5nardo@5e0d1d85-4f2b-0510-b5c6-998ec1f4411a>
2016-10-03 12:54:47 +0000

    1. Navigation 수정

--- Client_Root/Client/Assets/Scripts/Navigation/Common/Navigation.cs
+++ Client_Root/Client/Assets/Scripts/Navigation/Common/Navigation.cs
@@ -3,8 +3,15 @@ namespace Navigation
 {
 	public class Defines
 	{
-		public const int INVALID_NODE_INDEX = -1;
-		public const int NO_CLOSEST_NODE_FOUND = -1;
+		public const int 			INVALID_NODE_INDEX = -1;
+		public const int 			NO_CLOSEST_NODE_FOUND = -1;
+		public const double 		SMAllEST_DELAY = 0.25;
 	}
 
+	public enum SearchResult
+	{
+		target_found,
+		target_not_found,
+		search_incomplete,
+	}
 }
