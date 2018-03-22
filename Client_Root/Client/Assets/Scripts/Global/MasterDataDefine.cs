1052177b49d4b3f0c3f5c89b8cb20d477e9a2b50
Bae-PC\Bae <re5nardo@naver.com>
2018-03-22 23:43:28 +0900

    [배인수] 1. GameItem 추가 (Thunder)

--- Client_Root/Client/Assets/Scripts/Global/MasterDataDefine.cs
+++ Client_Root/Client/Assets/Scripts/Global/MasterDataDefine.cs
@@ -4,6 +4,6 @@ namespace MasterDataDefine
     public class GameItem
     {
         public const int FirstID = 0;
-        public const int LastID = 5;
+        public const int LastID = 6;
     }
 }
