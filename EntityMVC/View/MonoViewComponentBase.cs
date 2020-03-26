0f8048a8e3e2f25ad127c1bb90d490f26570bf35
re5nardo <re5nardo@naver.com>
2020-03-26 23:22:41 +0900

    [배인수] 1. GameFramework에 Base 컴포넌트들 추가

---
+++ EntityMVC/View/MonoViewComponentBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using GameFramework;
+
+public class MonoViewComponentBase : MonoBehaviour, IViewComponent
+{
+	public IEntity Entity { get; private set; }
+
+	public virtual void OnCommand(ICommand command)
+	{
+	}
+
+	public virtual void OnAttached(IEntity entity)
+	{
+		Entity = entity;
+	}
+
+	public virtual void OnDetached()
+	{
+		Entity = null;
+	}
+
+	public virtual void Initialize(params object[] param)
+	{
+	}
+}
