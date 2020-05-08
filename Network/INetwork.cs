65389940c77c75d590096d5575807bee6d483125
re5nardo <re5nardo@naver.com>
2020-05-08 23:20:00 +0900

    [배인수] 네이밍 수정

--- Network/INetwork.cs
+++ Network/INetwork.cs
@@ -8,7 +8,7 @@ namespace GameFramework
         INetworkImpl m_NetworkImpl { get; set; }
         Action<IMessage, object[]> onMessage { get; set; }
 
-        void Send(IMessage msg, int nTargetUserID, bool bReliable = true, bool bInstant = false);
+        void Send(IMessage msg, int nTargetID, bool bReliable = true, bool bInstant = false);
         void SendToAll(IMessage msg, bool bReliable = true, bool bInstant = false);
         void SendToNear(IMessage msg, Vector3 vec3Center, float fRadius, bool bReliable = true, bool bInstant = false);
     }
--- Network/INetworkImpl.cs
+++ Network/INetworkImpl.cs
@@ -7,7 +7,7 @@ namespace GameFramework
     {
         Action<IMessage, object[]> onMessage { get; set; }
 
-        void Send(IMessage msg, int nTargetUserID, bool bReliable = true, bool bInstant = false);
+        void Send(IMessage msg, int nTargetID, bool bReliable = true, bool bInstant = false);
         void SendToAll(IMessage msg, bool bReliable = true, bool bInstant = false);
         void SendToNear(IMessage msg, Vector3 vec3Center, float fRadius, bool bReliable = true, bool bInstant = false);
     }
