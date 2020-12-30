70b3780f7b5786619d85efd477a2476f7bbd7a54
re5nardo <re5nardo@naver.com>
2020-12-30 21:14:16 +0900

    [배인수] 1. 상태머신 구현

---
+++ Common/IFiniteStateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GameFramework
+{
+    namespace FSM
+    {
+        public interface IFiniteStateMachine<S, I> where S : IState<I> where I : Enum
+        {
+            S InitState { get; }
+            S CurrentState { get; }
+            S MoveNext(I input);    //  인풋값에 따라 상태 전이
+        }
+
+        public interface IState<I> where I : Enum
+        {
+            IFiniteStateMachine<IState<I>, I> FSM { get; }
+
+            void Enter();
+            void Execute();
+            void Exit();
+
+            IState<I> GetNext(I input); //  인풋값에 의한 다음 상태 값 반환
+        }
+    }
+}
