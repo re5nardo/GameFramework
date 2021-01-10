aa31d4b09ce8ef721dc0227950b9fd6f0dc44cb8
re5nardo <re5nardo@naver.com>
2021-01-10 20:30:30 +0900

    [배인수] 1. 상태머신 관련 소스 다듬기

--- Common/IFiniteStateMachine.cs
+++ Common/IFiniteStateMachine.cs
@@ -4,22 +4,22 @@ namespace GameFramework
 {
     namespace FSM
     {
-        public interface IFiniteStateMachine<S, I> where S : IState<I> where I : Enum
+        public interface IFiniteStateMachine
         {
-            S InitState { get; }
-            S CurrentState { get; }
-            S MoveNext(I input);    //  인풋값에 따라 상태 전이
+            IState InitState { get; }
+            IState CurrentState { get; }
+            IState MoveNext<I>(I input) where I : Enum;    //  인풋값에 따라 상태 전이
         }
 
-        public interface IState<I> where I : Enum
+        public interface IState
         {
-            IFiniteStateMachine<IState<I>, I> FSM { get; }
+            IFiniteStateMachine FSM { get; }
 
             void Enter();
             void Execute();
             void Exit();
 
-            IState<I> GetNext(I input); //  인풋값에 의한 다음 상태 값 반환
+            IState GetNext<I>(I input) where I : Enum; //  인풋값에 의한 다음 상태 값 반환
         }
     }
 }
