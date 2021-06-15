b85e781e0ebd3ff6ca82fef2abbb1ddee9fec79a
re5nardo <re5nardo@naver.com>
2021-06-16 01:06:04 +0900

    [배인수] 1. EntityManager 수정 - GenerateLocalEntityID 추가 - 변수 네이밍 수정

--- Manager/EntityManager.cs
+++ Manager/EntityManager.cs
@@ -7,55 +7,68 @@ namespace GameFramework
 {
     public class EntityManager : MonoBehaviour
     {
-        protected int m_nEntitySequence = 0;
-        protected Dictionary<int, IEntity> m_dicEntity = new Dictionary<int, IEntity>();
+        protected int entitySequence = 0;
+        protected int localEntitySequence = -1;
 
-        protected Grid m_PositionGrid = null;
+        protected Dictionary<int, IEntity> dicEntity = new Dictionary<int, IEntity>();
+
+        protected Grid positionGrid = null;
 
         public virtual void Clear()
         {
-            m_nEntitySequence = 0;
-            m_dicEntity.Clear();
+            entitySequence = 0;
+            dicEntity.Clear();
         }
 
+        /// <summary>
+        /// Generate (global) EntityID (Range: 0 ~ int.MaxValue)
+        /// </summary>
         public int GenerateEntityID()
         {
-            return m_nEntitySequence++;
+            return entitySequence++;
+        }
+
+        /// <summary>
+        /// Generate Local EntityID (Range: -1 ~ int.MinValue)
+        /// </summary>
+        public int GenerateLocalEntityID()
+        {
+            return localEntitySequence--;
         }
 
         public void RegisterEntity(IEntity entity)
         {
-            if (m_dicEntity.ContainsKey(entity.EntityID))
+            if (dicEntity.ContainsKey(entity.EntityID))
             {
                 Debug.LogError("EntityID already exists! EntityID : " + entity.EntityID);
                 return;
             }
 
-            m_dicEntity.Add(entity.EntityID, entity);
+            dicEntity.Add(entity.EntityID, entity);
 
-            m_PositionGrid.Add(entity.EntityID);
+            positionGrid.Add(entity.EntityID);
         }
 
         public bool IsRegistered(int nEntityID)
         {
-            return m_dicEntity.ContainsKey(nEntityID);
+            return dicEntity.ContainsKey(nEntityID);
         }
 
         public void UnregisterEntity(int nEntityID)
         {
-            if (!m_dicEntity.ContainsKey(nEntityID))
+            if (!dicEntity.ContainsKey(nEntityID))
             {
                 Debug.LogError("EntityID does not exist! EntityID : " + nEntityID);
                 return;
             }
 
-            m_PositionGrid.Remove(nEntityID);
-            m_dicEntity.Remove(nEntityID);
+            positionGrid.Remove(nEntityID);
+            dicEntity.Remove(nEntityID);
         }
 
         public IEntity GetEntity(int nEntityID)
         {
-            if (m_dicEntity.TryGetValue(nEntityID, out IEntity entity))
+            if (dicEntity.TryGetValue(nEntityID, out IEntity entity))
             {
                 return entity;
             }
@@ -66,7 +79,7 @@ namespace GameFramework
 
         public T GetEntity<T>(int nEntityID) where T : IEntity
         {
-            if (m_dicEntity.TryGetValue(nEntityID, out IEntity entity))
+            if (dicEntity.TryGetValue(nEntityID, out IEntity entity))
             {
                 if (entity is T target)
                 {
@@ -82,52 +95,52 @@ namespace GameFramework
 
         public List<IEntity> GetEntities(Vector3 vec3Position, float fRadius, List<Predicate<IEntity>> conditions)
         {
-            return m_PositionGrid.GetEntities(vec3Position, fRadius, conditions);
+            return positionGrid.GetEntities(vec3Position, fRadius, conditions);
         }
 
         public List<T> GetEntities<T>(Vector3 vec3Position, float fRadius, List<Predicate<IEntity>> conditions) where T : IEntity
         {
-            return m_PositionGrid.GetEntities(vec3Position, fRadius, conditions).Cast<T>().ToList();
+            return positionGrid.GetEntities(vec3Position, fRadius, conditions).Cast<T>().ToList();
         }
 
         public List<IEntity> GetEntities(Transform trTarget, float fFieldOfViewAngle, float fRadius, List<System.Predicate<IEntity>> conditions)
         {
-            return m_PositionGrid.GetEntities(trTarget, fFieldOfViewAngle, fRadius, conditions);
+            return positionGrid.GetEntities(trTarget, fFieldOfViewAngle, fRadius, conditions);
         }
 
         public List<T> GetEntities<T>(Transform trTarget, float fFieldOfViewAngle, float fRadius, List<System.Predicate<IEntity>> conditions) where T : IEntity
         {
-            return m_PositionGrid.GetEntities(trTarget, fFieldOfViewAngle, fRadius, conditions).Cast<T>().ToList();
+            return positionGrid.GetEntities(trTarget, fFieldOfViewAngle, fRadius, conditions).Cast<T>().ToList();
         }
 
         public List<IEntity> GetAllEntities()
         {
-            return new List<IEntity>(m_dicEntity.Values);
+            return new List<IEntity>(dicEntity.Values);
         }
 
         public List<T> GetAllEntities<T>() where T : IEntity
         {
-            return m_dicEntity.Values.Cast<T>().ToList();
+            return dicEntity.Values.Cast<T>().ToList();
         }
 
         public HashSet<int> GetAllEntityIDs()
         {
-            return new HashSet<int>(m_dicEntity.Keys);
+            return new HashSet<int>(dicEntity.Keys);
         }
 
         public HashSet<Cell> GetCells(Vector2Int vec2Center, float fRadius, bool bMakeCell = false)
         {
-            return m_PositionGrid.GetCells(vec2Center, fRadius, bMakeCell);
+            return positionGrid.GetCells(vec2Center, fRadius, bMakeCell);
         }
 
         public Cell GetCell(Vector2Int vec2CellPosition, bool bMakeCell = false)
         {
-            return m_PositionGrid.GetCell(vec2CellPosition, bMakeCell);
+            return positionGrid.GetCell(vec2CellPosition, bMakeCell);
         }
 
         public Vector2Int GetEntityCellPosition(int nEntityID)
         {
-            return m_PositionGrid.GetEntityCellPosition(nEntityID);
+            return positionGrid.GetEntityCellPosition(nEntityID);
         }
     }
 }
