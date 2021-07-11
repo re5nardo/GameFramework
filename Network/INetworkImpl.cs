aec5772aa6997cb12f2af112773d4bbc0954c88f
re5nardo <re5nardo@naver.com>
2021-07-11 15:06:06 +0900

    [배인수] 1. 네트워크 인터페이스 수정

--- Network/INetworkImpl.cs
+++ Network/INetworkImpl.cs
@@ -8,6 +8,7 @@ namespace GameFramework
         Action<IMessage> OnMessage { get; set; }
 
         void Send(IMessage msg, int targetId, bool reliable = true, bool instant = false);
+        void Send(IMessage msg, ulong targetId, bool reliable = true, bool instant = false);
         void SendToAll(IMessage msg, bool reliable = true, bool instant = false);
         void SendToNear(IMessage msg, Vector3 center, float radius, bool reliable = true, bool instant = false);
     }
